bfa51744d191cca60e1a809d7513e5e186ac3150
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-02 18:42:04 +0200

    convert serverhelp

--- src/serverhelp.js
+++ src/serverhelp.js
@@ -2,10 +2,16 @@
 export async function main(ns) {
   var attack_server = ns.args[0];
   var target_host = ns.args[1];
-  var threads = Math.floor((ns.getServerMaxRam(attack_server)-ns.getServerUsedRam(attack_server))/ns.getScriptRam("hack.js","home"));
-  ns.scp("hack.js", attack_server, "home");
-  ns.exec("hack.js",attack_server, threads,
+  var mem = (ns.getServerMaxRam(attack_server)-ns.getServerUsedRam(attack_server)- ns.getScriptRam("basic_hacking/hacking_controller.js"));
+
+  ns.scp(["basic_hacking/hacking_controller.js", "basic_hacking/hack.js", "basic_hacking/grow.js", "basic_hacking/weaken.js"], attack_server, "home");
+
+  ns.exec("basic_hacking/hacking_controller.js", attack_server, 1,
   target_host, 
   ns.getServerMaxMoney(target_host), 
-  ns.getServerMinSecurityLevel(target_host));
+  ns.getServerMinSecurityLevel(target_host),
+  mem,
+  ns.getScriptRam("basic_hacking/weaken.js"),
+  ns.getScriptRam("basic_hacking/grow.js"),
+  ns.getScriptRam("basic_hacking/hack.js"));
 }
