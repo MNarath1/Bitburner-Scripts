788a120d988591fc2851641319462866ee4a9c89
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-03 11:29:17 +0200

    add additional sleep to start of weaken,grow, hack etc to ensure they can be killed after game restart

--- src/basic_hacking/weaken.js
+++ src/basic_hacking/weaken.js
@@ -1,5 +1,6 @@
 /** @param {import("@ns").NS} ns */
 export async function main(ns) {
+    ns.sleep(1000);
     const port = ns.getPortHandle(ns.pid);
     port.write(await ns.weaken(ns.args[0]));
 }
