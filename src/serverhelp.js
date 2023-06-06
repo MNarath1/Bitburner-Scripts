3333882913014224da7ded78d9c062bc2d076046
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-06 21:35:11 +0200

    forgot serverhelper for scp helper

--- src/serverhelp.js
+++ src/serverhelp.js
@@ -1,9 +1,11 @@
+import { scp_helpers } from "./helper_functions/scp_helpers";
+
 /** @param {import("@ns").NS} ns */
 export async function main(ns) {
   var attack_server = ns.args[0];
   var target_host = ns.args[1];
   var mem = (ns.getServerMaxRam(attack_server)-ns.getServerUsedRam(attack_server)- ns.getScriptRam("basic_hacking/hacking_controller.js"));
-
+  scp_helpers(ns, "home", attack_server);
   ns.scp(["basic_hacking/hacking_controller.js", "basic_hacking/hack.js", "basic_hacking/grow.js", "basic_hacking/weaken.js"], attack_server, "home");
 
   ns.exec("basic_hacking/hacking_controller.js", attack_server, 1,
