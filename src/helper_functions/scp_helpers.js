49dbcaa59e4f1acd722384ed274ee78a5d23a15b
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-06 22:13:27 +0200

    doesn't need to be async

--- src/helper_functions/scp_helpers.js
+++ src/helper_functions/scp_helpers.js
@@ -1,5 +1,11 @@
-/** @param {import("@ns").NS} ns */
-export async function scp_helpers(ns, host, target) {
+/** 
+ * 
+ * @param {import("@ns").NS} ns 
+ * @param host:String Name of Host Server
+ * @param target:String Name of Destination Server
+ * 
+*/
+export function scp_helpers(ns, host, target) {
     for(let script of ns.ls(host,"helper_functions")) {
         ns.scp(script, target);
     }
