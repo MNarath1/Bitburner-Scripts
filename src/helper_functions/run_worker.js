9fc910fbfcfd4ca6a02766cd457daeb862be04cd
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-06 21:31:26 +0200

    implements worker runner script modules

---
+++ src/helper_functions/run_worker.js
@@ -0,0 +1,12 @@
+/** @param {import("@ns").NS} ns */
+export async function run_worker(ns, scriptname, threads) {
+    const pid = ns.run(scriptname, threads, target);
+    const port = ns.getPortHandle(pid);
+    await port.nextWrite();
+    if(port.peek() <= -1) {
+        port.clear();
+        throw new ErrorEvent("Unknown Error Script Terminated");
+      }
+    return port.read();
+    
+}
