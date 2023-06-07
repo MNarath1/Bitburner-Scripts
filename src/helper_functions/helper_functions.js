72d5133f0f112fb94f81b41ab90776879e2ce767
MNarath <47279840+MNarath1@users.noreply.github.com>
2023-06-07 23:20:00 +0200

    same as before making it temporary

--- src/helper_functions/helper_functions.js
+++ src/helper_functions/helper_functions.js
@@ -5,7 +5,8 @@
 
 /** @param {import("@ns").NS} ns */
 export async function run_worker(ns, scriptname, threads, target) {
-    const pid = ns.run(scriptname, threads, target);
+    const option = {temporary: true, threads: threads};
+    const pid = ns.run(scriptname, option, target);
     const port = ns.getPortHandle(pid);
     await port.nextWrite();
     if(port.peek() <= -1) {
