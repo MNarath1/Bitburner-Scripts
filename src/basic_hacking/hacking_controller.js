bca330f9ef22ce5835d9f6fc37a3659aa24517f3
MNarath <47279840+MNarath1@users.noreply.github.com>
2023-06-07 23:19:08 +0200

    maxes worker script temporary only

--- src/basic_hacking/hacking_controller.js
+++ src/basic_hacking/hacking_controller.js
@@ -31,7 +31,8 @@ export async function main(ns) {
 
 /** @param {import("@ns").NS} ns */
 export async function run_hacking_worker(ns, scriptname, threads, target) {
-  const pid = ns.run(scriptname, threads, target);
+  const option = {temporary: true, threads: threads};
+  const pid = ns.run(scriptname, option, target);
   const port = ns.getPortHandle(pid);
   await port.nextWrite();
   if(port.peek() <= -1) {
