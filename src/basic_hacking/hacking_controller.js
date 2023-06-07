ae44f623d53775b5f423b1b9844566408801e72e
mnarath1 <47279840+MNarath1@users.noreply.github.com>
2023-06-07 19:36:18 +0200

    just make it a second function instead of a shared one

--- src/basic_hacking/hacking_controller.js
+++ src/basic_hacking/hacking_controller.js
@@ -1,5 +1,3 @@
-import { run_worker } from "@/helper_functions/helper_functions";
-
 /** @param {import("@ns").NS} ns */
 export async function main(ns) {
     const target = ns.args[0];
@@ -14,13 +12,13 @@ export async function main(ns) {
         var security_level = ns.getServerSecurityLevel(target);
         var server_money = ns.getServerMoneyAvailable(target);  
         if(security_level > securityThresh) {
-            await run_worker(ns, "basic_hacking/weaken.js", Math.floor(mem/ns.args[4]), target);
+            await run_hacking_worker(ns, "basic_hacking/weaken.js", Math.floor(mem/ns.args[4]), target);
             // const data = port.read() //only needed if you want to grab data otherwise clear the port or so
         } else if(server_money < moneyThresh) {
-            await run_worker(ns, "basic_hacking/grow.js", Math.floor(mem/ns.args[5]), target);
+            await run_hacking_worker(ns, "basic_hacking/grow.js", Math.floor(mem/ns.args[5]), target);
         } else {
           try {
-            await run_worker(ns, "basic_hacking/hack.js", Math.floor(mem/ns.args[6]), target);
+            await run_hacking_worker(ns, "basic_hacking/hack.js", Math.floor(mem/ns.args[6]), target);
           } catch (ErrorEvent) {
             ns.print("Cannot execute hack waiting....");
             await ns.sleep(1000*60);
@@ -28,4 +26,18 @@ export async function main(ns) {
           }
         }
     }
+}
+
+
+/** @param {import("@ns").NS} ns */
+export async function run_hacking_worker(ns, scriptname, threads, target) {
+  const pid = ns.run(scriptname, threads, target);
+  const port = ns.getPortHandle(pid);
+  await port.nextWrite();
+  if(port.peek() <= -1) {
+      port.clear();
+      throw new ErrorEvent("Unknown Error Script Terminated");
+    }
+  return port.read();
+  
 }
